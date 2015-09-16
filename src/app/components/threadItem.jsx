719f6d7cdcdef86ecf1d10002c5e01a25a2cbbad
Luca Costa <zapduke@gmail.com>
2015-09-16 20:32:06 +0200

    Open Links on Touch

--- src/app/components/threadItem.jsx
+++ src/app/components/threadItem.jsx
@@ -2,19 +2,25 @@ let React = require('react');
 import {List, ListItem, ListDivider, Avatar} from 'material-ui';
 let Story = require('./story.jsx'); // Our custom react component
 
-let ThreadItem = React.createClass({render() {
+let ThreadItem = React.createClass({
+
+  render() {
     let thread = this.props.data.data,
       date = new Date((thread.created_utc * 1000)),
       secondaryText = `${thread.subreddit}  ${thread.author}  ${thread.created_utc} ${thread.num_comments} comments`
 
     return (
-      <ListItem primaryText={
-          <Story thumbnail={thread.thumbnail}
-            title={thread.title}
-            subtitle={secondaryText}
-            score={thread.score} />}
-            />
+      <ListItem onTouchTap={this.openStory}
+        primaryText={<Story thumbnail={thread.thumbnail}
+        title={thread.title}
+        subtitle={secondaryText}
+        score={thread.score} />}
+        />
     );
+  },
+
+  openStory() {
+    location.href = this.props.data.data.url
   }
 });
 
