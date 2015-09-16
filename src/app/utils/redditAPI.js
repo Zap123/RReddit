f8c0be0dc00d39626e27ab3d40fcafff80dea2da
ga2arch <gabriele.carrettoni@gmail.com>
2015-09-17 00:36:29 +0200

    indentation

--- src/app/utils/redditAPI.js
+++ src/app/utils/redditAPI.js
@@ -2,14 +2,14 @@ import request from 'superagent'
 
 export default class redditAPI {
 
-  static getHome() {
-      return new Promise((resolve, reject) => {
-          request.get('https://www.reddit.com/.json').end((err, res) => {
-              if (err)
-                  reject(err)
-              else
-                  resolve({data: res.body.data.children})
-              })
-          })
-      }
+    static getHome() {
+        return new Promise((resolve, reject) => {
+            request.get('https://www.reddit.com/.json').end((err, res) => {
+                if (err)
+                    reject(err)
+                else
+                    resolve({data: res.body.data.children})
+            })
+        })
+    }
 }
