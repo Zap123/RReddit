2a95c15d9756aae4329678fee4bae411bf1f0bdb
ga2arch <gabriele.carrettoni@gmail.com>
2015-09-16 21:56:10 +0200

    refactored Story

--- src/app/components/story.jsx
+++ src/app/components/story.jsx
@@ -1,33 +1,33 @@
-let React = require('react');
-let Thumbnail = require('./thumbnail.jsx'); // Our custom react component
-let Upvotes = require('./upvote.jsx'); // Our custom react component
-let Story = React.createClass({render() {
-    let styles = {
-      display: 'flex',
-      padding: '4px',
-      alignItem: 'center'
+import React     from 'react'
+import Thumbnail from './thumbnail.jsx'
+import Upvotes   from './upvote.jsx'
+
+export default class Story extends React.Component {
+
+    render() {
+        let itemStyle = {
+          display: 'flex',
+          padding: '4px',
+          alignItem: 'center'
+        }
+
+        let infoStyle = {
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          flex:'1'
+        }
+
+        return (
+          <div style={itemStyle}>
+            <Upvotes score={this.props.score}/>
+            <Thumbnail src={this.props.thumbnail}/>
+            <div style={infoStyle}>
+              <em>{this.props.title}</em>
+              <p>{this.props.subtitle}</p>
+            </div>
+
+          </div>
+        )
     }
-
-    let styles2 = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      flex:'1'
-    }
-
-    return (
-      <div style={styles}>
-        <Upvotes score={this.props.score}/>
-        <Thumbnail src={this.props.thumbnail}/>
-        <div style={styles2}>
-          <em>{this.props.title}</em>
-          <p>{this.props.subtitle}</p>
-        </div>
-
-      </div>
-    )
-  }
-
-})
-
-module.exports = Story;
+}
